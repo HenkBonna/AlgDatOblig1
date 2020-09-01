26af8df3a63bdbdb594b1a1a019bdd9fcb8aa30e
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2020-09-01 14:48:40 +0200

    Made 7a, b functional.

--- src/algdat/Main.java
+++ src/algdat/Main.java
@@ -3,6 +3,22 @@ package algdat;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Henk tests //
+        System.out.println("Test av 7a: ");
+        System.out.println(Oblig1.flett("ABCD","abcdefgh"));
+        System.out.println(Oblig1.flett("ABCDEFGHIS","abcdefgh"));
+        System.out.println(Oblig1.flett("ABCD","abcd"));
+        System.out.println(Oblig1.flett("ABCDEFGHIJKLMNO","abcd"));
+        System.out.println(Oblig1.flett("",""));
+        System.out.println();
+        System.out.println("Test av 7b:");
+        System.out.println(Oblig1.flett("AM ","L","GEDS","ORATKRR","","R TRTE","IO","TGAUU"));
+        System.out.println(Oblig1.flett("AAA","aaa","cccCCCC", "444"));
+
+
+
+
+        ////////////////
     }
 }
--- src/algdat/Oblig1.java
+++ src/algdat/Oblig1.java
@@ -41,19 +41,92 @@ public class Oblig1 {
         throw new UnsupportedOperationException();
     }
 
-    ///// Oppgave 7 //////////////////////////////////////
+    ///// Oppgave 7 - Henk //////////////////////////////////////
     /// 7a)
     public static String flett(String s, String t) {
-        throw new UnsupportedOperationException();
-        
+
+        //TODO this throw below me
+        //throw new UnsupportedOperationException();
+
+        char[] charArray_s = s.toCharArray();
+        char[] charArray_t = t.toCharArray();
+
+        char[] outArray = new char[charArray_s.length + charArray_t.length];
+
+
+        ///////
+        int shortestLength = charArray_s.length;
+        int longestLength = 0; // Temp value
+
+        if(charArray_t.length < charArray_s.length){       // This if-else simply assigns the longest lenghts to variables
+            shortestLength = charArray_t.length;
+            longestLength = charArray_s.length;
+        }
+        else{
+            longestLength = charArray_t.length;
+        }
+
+        ///////
+
+        for (int i = 0; i < shortestLength; ++i){   // This block assigns the initial values
+            outArray[2*i] = charArray_s[i];     // Using this type of indexing, we assign to every other value of out
+            outArray[2*i + 1] = charArray_t[i];
+        }
+
+        char[] restArray = new char[longestLength - shortestLength];    // Array to contain the rest of elements;
+        if (charArray_s.length < charArray_t.length){
+            for (int i = shortestLength; i < longestLength; ++i){
+                restArray[i-shortestLength] = charArray_t[i];
+            }
+        }
+        if(charArray_s.length > charArray_t.length){
+            for (int i = shortestLength; i < longestLength; ++i){
+                restArray[i-shortestLength] = charArray_s[i];
+            }
+        }
+
+        for(int i = 0; i < restArray.length; ++i){  // This block fills the rest
+            outArray[shortestLength*2 + i] = restArray[i];
+        }
+
+
+        for (int i = 0; i < charArray_s.length; ++i){
+
+        }
+        String out = "";
+        for (char c : outArray){
+            out += c;
+        }
+        return out;
     }
 
     /// 7b)
     public static String flett(String... s) {
-        throw new UnsupportedOperationException();
+
+        //TODO this throw below me
+        //throw new UnsupportedOperationException();
+
+
+        String out = "";
+        int totalLength = 0;
+        for(String str : s){
+            totalLength += str.length();    // This calculates the totalLength -> Could perhaps have a while statement instead, in the loop, would save costs
+        }
+
+        for (int j=0; j < totalLength; ++j) {       // This double if block loops through each word, getting the relevant
+            for (int i = 0; i < s.length; ++i) {    // letter. The trick is increasing 'i' before 'j' ( s[0][0] -> s[1][0] ...)
+                if(s[i].length() > j){
+                    out += s[i].toCharArray()[j];
+                }
+            }
+        }
+
+
+
+        return out;
     }
 
-    ///// Oppgave 8 //////////////////////////////////////
+    ///// Oppgave 8 - Henk //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
         throw new UnsupportedOperationException();
     }
