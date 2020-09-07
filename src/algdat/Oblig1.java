3ed04a03900d286bc29e3859fac7255528e9d090
Patrick Sletvold <patricksletvold@hotmail.com>
2020-09-07 16:51:14 +0200

    Formatering

--- src/algdat/Oblig1.java
+++ src/algdat/Oblig1.java
@@ -2,22 +2,19 @@ package algdat;
 
 ////// Losningsforslag Oblig 1 ////////////////////////
 
-import java.lang.UnsupportedOperationException;
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
-    private Oblig1() {}
+    private Oblig1() {
+    }
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
-        if (a.length == 0){
+        if (a.length == 0) {
             throw new NoSuchElementException("Det finnes ingen elementer i arrayet!");
-        }
-        else if(a.length == 1){
+        } else if (a.length == 1) {
             return a[0];
-        }
-        else {
+        } else {
             int[] temp = new int[1];
             for (int i = 1; i < a.length; i++) {
                 if (a[i] < a[i - 1]) {
@@ -32,13 +29,11 @@ public class Oblig1 {
 
     public static int ombyttinger(int[] a) {
         int ombyttinger = 0;
-        if (a.length == 0){
+        if (a.length == 0) {
             throw new NoSuchElementException("Det finnes ingen elementer i arrayet!");
-        }
-        else if(a.length == 1){
+        } else if (a.length == 1) {
             return 0;
-        }
-        else {
+        } else {
             int[] temp = new int[1];
             for (int i = 1; i < a.length; i++) {
                 if (a[i] < a[i - 1]) {
@@ -54,15 +49,14 @@ public class Oblig1 {
 
     ///// Oppgave 2 //////////////////////////////////////
     public static int antallUlikeSortert(int[] a) {
-        if (a.length == 0){
+        if (a.length == 0) {
             return 0;
         }
         int antallUlike = 1;
-        for(int i = 1; i < a.length; i++){
-            if (a[i] < a[i-1]){
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i - 1]) {
                 throw new IllegalStateException("Arrayet er ikke sortert!");
-            }
-            else if (a[i] > a[i-1]){
+            } else if (a[i] > a[i - 1]) {
                 antallUlike++;
             }
         }
@@ -71,23 +65,23 @@ public class Oblig1 {
 
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
-        if(a.length == 0){
+        if (a.length == 0) {
             return 0;
-        } else if (a.length == 1){
+        } else if (a.length == 1) {
             return 1;
         }
 
         int different = 1;
         boolean seen = false;
 
-        for(int i = 1; i < a.length; i++){
-            for (int j = 0; j < i; j++){
-                if(a[j] == a[i]){
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] == a[i]) {
                     seen = true;
                     break;
                 }
             }
-            if (!seen){
+            if (!seen) {
                 different++;
             } else {
                 seen = false;
@@ -205,43 +199,42 @@ public class Oblig1 {
         int shortestLength = charArray_s.length;
         int longestLength = 0; // Temp value
 
-        if(charArray_t.length < charArray_s.length){       // This if-else simply assigns the longest lenghts to variables
+        if (charArray_t.length < charArray_s.length) {       // This if-else simply assigns the longest lenghts to variables
             shortestLength = charArray_t.length;
             longestLength = charArray_s.length;
-        }
-        else{
+        } else {
             longestLength = charArray_t.length;
         }
 
         ///////
 
-        for (int i = 0; i < shortestLength; ++i){   // This block assigns the initial values
-            outArray[2*i] = charArray_s[i];     // Using this type of indexing, we assign to every other value of out
-            outArray[2*i + 1] = charArray_t[i];
+        for (int i = 0; i < shortestLength; ++i) {   // This block assigns the initial values
+            outArray[2 * i] = charArray_s[i];     // Using this type of indexing, we assign to every other value of out
+            outArray[2 * i + 1] = charArray_t[i];
         }
 
         char[] restArray = new char[longestLength - shortestLength];    // Array to contain the rest of elements;
-        if (charArray_s.length < charArray_t.length){
-            for (int i = shortestLength; i < longestLength; ++i){
-                restArray[i-shortestLength] = charArray_t[i];
+        if (charArray_s.length < charArray_t.length) {
+            for (int i = shortestLength; i < longestLength; ++i) {
+                restArray[i - shortestLength] = charArray_t[i];
             }
         }
-        if(charArray_s.length > charArray_t.length){
-            for (int i = shortestLength; i < longestLength; ++i){
-                restArray[i-shortestLength] = charArray_s[i];
+        if (charArray_s.length > charArray_t.length) {
+            for (int i = shortestLength; i < longestLength; ++i) {
+                restArray[i - shortestLength] = charArray_s[i];
             }
         }
 
-        for(int i = 0; i < restArray.length; ++i){  // This block fills the rest
-            outArray[shortestLength*2 + i] = restArray[i];
+        for (int i = 0; i < restArray.length; ++i) {  // This block fills the rest
+            outArray[shortestLength * 2 + i] = restArray[i];
         }
 
 
-        for (int i = 0; i < charArray_s.length; ++i){
+        for (int i = 0; i < charArray_s.length; ++i) {
 
         }
         String out = "";
-        for (char c : outArray){
+        for (char c : outArray) {
             out += c;
         }
         return out;
@@ -258,18 +251,17 @@ public class Oblig1 {
 
         String out = "";
         int totalLength = 0;
-        for(String str : s){
+        for (String str : s) {
             totalLength += str.length();    // This calculates the totalLength -> Could perhaps have a while statement instead, in the loop, would save costs
         }
 
-        for (int j=0; j < totalLength; ++j) {       // This double if block loops through each word, getting the relevant
+        for (int j = 0; j < totalLength; ++j) {       // This double if block loops through each word, getting the relevant
             for (int i = 0; i < s.length; ++i) {    // letter. The trick is increasing 'i' before 'j' ( s[0][0] -> s[1][0] ...)
-                if(s[i].length() > j){
+                if (s[i].length() > j) {
                     out += s[i].toCharArray()[j];
                 }
             }
         }
-
 
 
         return out;
@@ -283,9 +275,9 @@ public class Oblig1 {
 
         // TODO: make tests to see if it ACTUALLY works
 
-        if(a.length == 0){
+        if (a.length == 0) {
             return new int[]{};
-        } else if(a.length == 1){
+        } else if (a.length == 1) {
             return new int[]{0};
         }
 
@@ -300,15 +292,15 @@ public class Oblig1 {
 
         int[] indicesSorted = new int[a.length];    // Create an empty array to be filled with the sorted indices
 
-        for(int i = 0; i < indicesSorted.length; ++i){  // For each position in the empty array,
+        for (int i = 0; i < indicesSorted.length; ++i) {  // For each position in the empty array,
             int minIndex = 0;
-            for (int j = 1; j < temp.length; ++j){      // we search for the smallest value
-                if (temp[j] < temp[minIndex]){
+            for (int j = 1; j < temp.length; ++j) {      // we search for the smallest value
+                if (temp[j] < temp[minIndex]) {
                     minIndex = j;
                 }
             }
             indicesSorted[i] = minIndex;                // and add it to the current position.
-            temp[minIndex] = max_val+1;                 // Finally, we (hackily) assure that this value won't be the next.
+            temp[minIndex] = max_val + 1;                 // Finally, we (hackily) assure that this value won't be the next.
         }
         // If we were to look at the temp array, we would see that all it's values are larger than –or equal to–
         // the largest entry in a. This is a sorta hacky solution, to be sure, but it works.
@@ -319,20 +311,20 @@ public class Oblig1 {
 
     ///// Oppgave 9 //////////////////////////////////////
     public static int[] tredjeMin(int[] a) {
-        if (a.length < 3){
+        if (a.length < 3) {
             throw new NoSuchElementException("Array has to few elements");
         }
         int[] begynnelse = {a[0], a[1], a[2]};
         begynnelse = indekssortering(begynnelse);
         int minIndex = begynnelse[0], nestMinIndex = begynnelse[1], tredjeMinIndex = begynnelse[2],
-        min = a[minIndex], nestMin = a[nestMinIndex], tredjeMin = a[tredjeMinIndex];
+                min = a[minIndex], nestMin = a[nestMinIndex], tredjeMin = a[tredjeMinIndex];
 
-        for (int i = 3; i < a.length; i++){
-            if (a[i] < tredjeMin){
+        for (int i = 3; i < a.length; i++) {
+            if (a[i] < tredjeMin) {
 
-                if (a[i] < nestMin){
-                    
-                    if (a[i] < min){ //nytt minstetall
+                if (a[i] < nestMin) {
+
+                    if (a[i] < min) { //nytt minstetall
                         tredjeMin = nestMin; //flytter nestminst til tredjeminst
                         nestMin = min; //flytter forrige minst til nestminst
                         min = a[i]; //oppdaterer minst
